1dc6d42a94aa67b3f8c9f4677fdd6662254f1b5d
Tim Snowhite <tim@snowhitesolutions.com>
2014-05-22 15:49:08 -0400

    Use a horizontal ray, as in http://rosettacode.org/wiki/Ray-casting_algorithm

--- lib/algorithms/point_in_polygon.rb
+++ lib/algorithms/point_in_polygon.rb
@@ -18,7 +18,7 @@ module Geometry
       return :outside unless bounding_box.contains?(point)
       return :on_the_boundary if point_is_vertex? || point_on_edge?
 
-      intersection_count(choose_good_ray).odd? ? :inside : :outside
+      intersection_count(Segment(point, Point(point.x + sufficient_ray_radius, point.y))).odd? ? :inside : :outside
     end
 
     delegate :vertices, :edges, :bounding_box, :to => :polygon
@@ -33,28 +33,9 @@ module Geometry
     def point_on_edge?
       edges.any? { |edge| edge.contains_point?(point) }
     end
-
-    def choose_good_ray
-      ray = random_ray
-      while ! good_ray?(ray) do
-        ray = random_ray
-      end
-      ray
-    end
-
-    def good_ray?(ray)
-      edges.none? { |edge| edge.parallel_to?(ray) } && vertices.none? { |vertex| ray.contains_point?(vertex) }
-    end
     
     def intersection_count(ray)
       edges.select { |edge| edge.intersects_with?(ray) }.size
-    end
-    
-    def random_ray
-      random_direction = rand * (2 * Math::PI)
-      
-      ray_endpoint = Point sufficient_ray_radius * Math.cos(random_direction), sufficient_ray_radius * Math.sin(random_direction)
-      Segment point, ray_endpoint
     end
 
     def sufficient_ray_radius
